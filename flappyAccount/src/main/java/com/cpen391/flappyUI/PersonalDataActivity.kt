51b41d342008f4b1e023da758935589222d4a10e
Robin Lai <robinlaiyy@gmail.com>
2021-04-03 19:23:24 +0800

    add current score

--- flappyAccount/src/main/java/com/cpen391/flappyUI/PersonalDataActivity.kt
+++ flappyAccount/src/main/java/com/cpen391/flappyUI/PersonalDataActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import com.cpen391.appbase.ui.mvvm.MvvmActivity
 import com.cpen391.flappyaccount.databinding.ActivityPersonalDataBinding
+import com.cpen391.flappyaccount.model.api.LoggedInUser
 
 class PersonalDataActivity  : MvvmActivity<ActivityPersonalDataBinding>() {
     private val context: Context = this
@@ -13,11 +14,11 @@ class PersonalDataActivity  : MvvmActivity<ActivityPersonalDataBinding>() {
         super.onCreate(savedInstanceState)
         val actionBar: ActionBar = supportActionBar!!
         actionBar.hide()
-        binding.userName.text = "a"
-        binding.userEmail.text = "b"
-        binding.top1Score.text = "b"
-        binding.top2Score.text = "b"
-        binding.top3Score.text = "b"
+        binding.userName.text = LoggedInUser.user.fullName
+        binding.userEmail.text = LoggedInUser.user.email
+        binding.top1Score.text = LoggedInUser.user.top_three_scores[0].toString()
+        binding.top2Score.text = LoggedInUser.user.top_three_scores[1].toString()
+        binding.top3Score.text = LoggedInUser.user.top_three_scores[2].toString()
     }
 
     override fun initObserver() {
