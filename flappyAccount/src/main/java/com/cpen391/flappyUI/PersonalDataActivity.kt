fce10c062c143af13331da4f57d567c101808748
MaxonZhao <maxonzhao@gmail.com>
2021-04-04 10:24:43 -0700

    fix: user top 3 scores not updated after game end

--- flappyAccount/src/main/java/com/cpen391/flappyUI/PersonalDataActivity.kt
+++ flappyAccount/src/main/java/com/cpen391/flappyUI/PersonalDataActivity.kt
@@ -2,9 +2,18 @@ package com.cpen391.flappyUI
 
 import android.content.Context
 import android.os.Bundle
+import androidx.annotation.MainThread
 import androidx.appcompat.app.ActionBar
+import com.cpen391.appbase.network.SimpleObserver
 import com.cpen391.appbase.ui.mvvm.MvvmActivity
+import com.cpen391.flappyaccount.Injection
+import com.cpen391.flappyaccount.consts.LoginStatus
 import com.cpen391.flappyaccount.databinding.ActivityPersonalDataBinding
+import com.cpen391.flappyaccount.model.bean.User
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class PersonalDataActivity  : MvvmActivity<ActivityPersonalDataBinding>() {
     private val context: Context = this
@@ -13,6 +22,28 @@ class PersonalDataActivity  : MvvmActivity<ActivityPersonalDataBinding>() {
         super.onCreate(savedInstanceState)
         val actionBar: ActionBar = supportActionBar!!
         actionBar.hide()
+
+        lateinit var user: User
+
+        LoggedInUser.instance?.getUser()?.let {
+            Injection.provideUserRepository()
+                .findUser(it.userName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : SimpleObserver<User>() {
+
+                    override fun onError(e: Throwable) {
+                        Timber.d("please connect to network")
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                    }
+
+                    override fun whenSuccess(t: User) {
+                        user = it
+                    }
+                })
+        }
 
         binding.apply {
 
