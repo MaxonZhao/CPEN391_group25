dd68a9cfa4df01365d816f6085561350f396170b
Zhuoyi Li <zhuoyili20170611@outlook.com>
2021-04-04 22:01:45 -0700

    fix: fix the problem that access point can't be connected after reboot

--- lua_script/init.lua
+++ lua_script/init.lua
@@ -4,6 +4,7 @@
 
 
 --connect to Access Point 
+wifi.setmode(wifi.STATION)
 station_cfg={}
 station_cfg.ssid="TELUS5905"
 station_cfg.pwd="7xtjr8njb3"
@@ -11,12 +12,13 @@ station_cfg.save=true --set to true means save config to flash
 wifi.sta.config(station_cfg)
 wifi.sta.autoconnect(1)
 
-tmr.delay(1000000) --wait 2,000,000 us = 2 second
+tmr.delay(2000000) --wait 2,000,000 us = 2 second
 
 --set up DNS server for the wifi dongle
 net.dns.setdnsserver("8.8.8.8", 0) --the primary DNS server the wifi dongle is gonning to use
 net.dns.setdnsserver("192.168.1.254", 1) --the secondary DNS server
 
+
 --print out connection status and ip to check if successfully connected.
---print(wifi.sta.status())
---print(wifi.sta.getip())
+print(wifi.sta.status())
+print(wifi.sta.getip())
