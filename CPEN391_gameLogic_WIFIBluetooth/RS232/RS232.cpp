d200d9fe8d13418f60c9ba5a66139fe123decf38
zoey1231 <54882896+zoey1231@users.noreply.github.com>
2021-04-12 22:09:14 -0700

    Update RS232.cpp
    chore: delete debugging print statements

--- CPEN391_gameLogic_WIFIBluetooth/RS232/RS232.cpp
+++ CPEN391_gameLogic_WIFIBluetooth/RS232/RS232.cpp
@@ -24,28 +24,25 @@ void Init_RS232(volatile unsigned char* LineControlReg, volatile unsigned char* 
 		volatile unsigned char* DivisorLatchMSB, volatile unsigned char* FifoControlReg){
     // set bit 7 of Line Control Register to 1, to gain access to the baud rate registers
 	*LineControlReg = 0x80;
-//    printf("LineControlReg:%X\n",*LineControlReg);
+
     // set Divisor latch (LSB and MSB) with correct value for required baud rate
     //baud rate divisor value=freq of BR_clk/ (desired buad rate*16)
     //here buad rate = 38400(0x51 and 0x00) 57600(0x36 and 0x00) (0x1B and 0x00 for 115200 )
 
 	*DivisorLatchLSB = 0x1B;
 	*DivisorLatchMSB = 0x00;
-//    printf("DivisorLatchLSB:%X\n",*DivisorLatchLSB);
-//    printf("DivisorLatchMSB:%X\n",*DivisorLatchMSB);
+
     // set bit 7 of Line control register back to 0 and
     // program other bits in that reg for 8 bit data, 1 stop bit, no parity etc
     //bit 1-0 = 11, bit 2 =0, bit 3 = 0
     *LineControlReg = 0x03;
-//    printf("LineControlReg:%X\n",*LineControlReg);
+
     // Reset the Fifo in the FiFo Control Reg by setting bits 1 & 2 to 1
     *FifoControlReg = 0x07;
-//    printf("DivisorLatchLSB:%X\n",*DivisorLatchLSB);
-//    printf("DivisorLatchMSB:%X\n",*DivisorLatchMSB);
-//    printf("FifoControlReg:%X\n",*FifoControlReg);
+
     // Now Clear all bits in the FiFo control registers
     *FifoControlReg = 0x01;
-//    printf("FifoControlReg:%X\n",*FifoControlReg);
+
 }
 
 // the following function polls the UART to determine if any character
@@ -54,7 +51,6 @@ void Init_RS232(volatile unsigned char* LineControlReg, volatile unsigned char* 
 int RS232_TestForReceivedData(volatile unsigned char* LineStatusReg){
     // if BT_LineStatusReg bit 0 is set to 1
     //return TRUE, otherwise return FALSE
-	//printf("*LineStatusReg:%X\n",*LineStatusReg);
     if((*LineStatusReg & 0x01)== 0x01){
         return 1;
     }
@@ -66,9 +62,7 @@ int putcharRS232(int c, volatile unsigned char* LineStatusReg,  volatile unsigne
     // wait for Transmitter Holding Register bit (5) of line status register to be '1'
     // indicating we can write to the device
     while((*LineStatusReg & 0x20)!= 0x20);
-//    printf("*LineStatusReg:%X\n",*LineStatusReg);
-//    printf("*TransmitterFifo:%X\n",*TransmitterFifo);
-//        printf("char:%c\n",c);
+
     // write character to Transmitter fifo register
     *TransmitterFifo = c;
     // return the character we printed
@@ -97,7 +91,7 @@ void RS232_Flush(volatile unsigned char* ReceiverFifo, volatile unsigned char* L
     }
 
     return;
-    // no more characters so return
+
 }
 
 
