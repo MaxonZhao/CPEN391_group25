d571679b78a1cfdf2c21a77da252aed1a2365343
zoey1231 <54882896+zoey1231@users.noreply.github.com>
2021-04-14 00:10:36 -0700

    style: add comments for communication tests

--- DS-5 Workspace/MyCPEN391Project/main.c
+++ DS-5 Workspace/MyCPEN391Project/main.c
@@ -9,6 +9,20 @@
 #include "serial.h"
 #include "wifi.h"
 
+/**
+* This file is a testbench used for testing if the WiFi serial communication and 
+* the Bluetooth serial Communication is working before integrate with game logic codes.
+*/
+
+/**
+* Test if we can successfully send a message from the RFS board to an blueTooth device(e.g. an Android mobile phone)
+* and if we can successfully read a message from an blueTooth device.
+* The test can be done in 2 ways:
+*	with a physical Android phone through a Bluetooth serial terminal app avaliable in 
+* 	Google Play Store(used in the early developement stage). Tests can also be done with the actual Andriod App
+* 	we develop in the later integration stage.
+*	with Python codes(in BlueTooth_hw_test folder) that simulates the player generates the signal in a random time interval to let the bird jump. 
+*/
 int testBTCommunication(){
  	int count = 0;
      Init_RS232(BT_LineControlReg, BT_DivisorLatchLSB, BT_DivisorLatchMSB, BT_FifoControlReg);
@@ -18,20 +32,23 @@ int testBTCommunication(){
 
      printf("Bluetooth initialized\n");
 
-//     sendMessage("Hello from arm to device 1",BT_LineStatusReg, BT_TransmitterFifo);
-     sendMessage("Hello from arm to device 2",BT2_LineStatusReg, BT2_TransmitterFifo);
+    sendMessage("Hello from arm to device 1",BT_LineStatusReg, BT_TransmitterFifo);
+//      sendMessage("Hello from arm to device 2",BT2_LineStatusReg, BT2_TransmitterFifo);
 
  	for(int i=0;i<1;i++){
  		count++;
-// 		char data_r1 = getcharRS232(BT_ReceiverFifo,BT_LineStatusReg);
-// 		printf("%d receive from device 1 in ascii:(hex) %X,(char) %c\n",count, data_r1,data_r1);
- 		char data_r2 = getcharRS232(BT2_ReceiverFifo,BT2_LineStatusReg);
- 		printf("%d receive from device 1 in ascii:(hex) %X,(char) %c\n",count, data_r2,data_r2);
+		char data_r1 = getcharRS232(BT_ReceiverFifo,BT_LineStatusReg);
+		printf("%d receive from device 1 in ascii:(hex) %X,(char) %c\n",count, data_r1,data_r1);
+//  		char data_r2 = getcharRS232(BT2_ReceiverFifo,BT2_LineStatusReg);
+//  		printf("%d receive from device 1 in ascii:(hex) %X,(char) %c\n",count, data_r2,data_r2);
  	}
  	 sendMessage("44",BT2_LineStatusReg, BT2_TransmitterFifo);
 
  }
 
+/**
+* Test if we can successfully invoke the lua script inside the RFS board's Wifi Chip to update the current score for user "test" to 44.
+*/
 int testUploadScore(){
 
 	Init_RS232(WIFI_LineControlReg, WIFI_DivisorLatchLSB, WIFI_DivisorLatchMSB, WIFI_FifoControlReg);
@@ -47,6 +64,9 @@ int testUploadScore(){
 	}
 
 }
+/**
+* Main function to run tests.
+*/
 int main(void){
 
 	testBTCommunication();
